f7fca10d935bff700beca11f8a825dc8080ade2f
Nancy Zaky <nancyzki@yahoo.com>
2021-10-28 22:45:41 -0700

    modidfied backend url

--- client/src/Checkout.js
+++ client/src/Checkout.js
@@ -14,11 +14,12 @@ const Checkout = ({
   changeErrorMessage,
 }) => {
   const [success, setSucces] = useState(false);
+  const [points, setPoints] = useState(0);
   useEffect(() => {
     handleBook();
   }, []);
   const handleCart = () => {
-    fetch("/bookings", {
+    fetch("/api/bookings", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -32,6 +33,7 @@ const Checkout = ({
       if (resp.ok) {
         resp.json().then((d) => {
           setSucces(true);
+          setPoints(100 * daysCount);
         });
       } else {
         resp.json().then((errors) => {
@@ -51,29 +53,39 @@ const Checkout = ({
         </span>
         {!startDate && <p>please select a start and end date</p>}
         {user.name && startDate && (
-          <section>
-            <h4>{`Hi ${user.name}`}</h4>
+          <section style={{ textAlign: "center" }}>
+            <h4
+              style={{ color: "red", marginBottom: "2rem" }}
+            >{`Hi ${user.name}`}</h4>
 
-            <p>{`You selected ${daysCount} days, Dates between ${startDate
+            <h5>{`You selected ${daysCount} days, Dates between ${startDate
               .toString()
-              .slice(0, 16)} and ${endDate.toString().slice(0, 16)}`}</p>
+              .slice(0, 16)} and ${endDate.toString().slice(0, 16)}`}</h5>
+            <h4>{`Total price for ${daysCount} days = $${
+              daysCount * room.price
+            }`}</h4>
+            <button className="btn" onClick={handleCart}>
+              Check Out
+            </button>
           </section>
         )}
 
         {/* {booked && <h3>awesome!! Dates are available</h3>} */}
 
-        <h4>{`Total price for ${daysCount} days = ${
-          daysCount * room.price
-        }`}</h4>
-
-        <button className="btn" onClick={handleCart}>
-          Add To Cart
-        </button>
-
         {success && (
-          <h1>{`Congratulations ${user.name} your Booking is Confirmed`}</h1>
+          <>
+            <h3
+              style={{ color: "green", textAlign: "center" }}
+            >{`Congratulations ${user.name} your Booking is Confirmed`}</h3>
+            <h4
+              style={{ color: "blue", textAlign: "center", paddingTop: "1rem" }}
+            >
+              {`You earned X${points} points `}{" "}
+            </h4>
+          </>
         )}
-        <h4 style={{ color: "red" }}>{errorMessage}</h4>
+
+        <h4 style={{ color: "red", marginTop: "5rem" }}>{errorMessage}</h4>
       </aside>
     </div>
   );
