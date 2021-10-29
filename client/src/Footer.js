1919dd8e920f4a65adfa87ef7e6293201eedcdaa
Nancy Zaky <nancyzki@yahoo.com>
2021-10-28 22:59:50 -0700

    changed back ground

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer style={{ backgroundColor: "grey", height: "200px" }}></footer>;
+  return (
+    <footer
+      style={{
+        backgroundColor: "black",
+        height: "200px",
+        // marginTop: "100rem",
+        width: "100%",
+      }}
+    >
+      <h1
+        style={{
+          textAlign: "center",
+          color: "white",
+          paddingTop: "3rem",
+          fontFamily: "Snell Roundhand, cursive",
+          fontSize: "30px",
+          fontWeight: "bolder",
+        }}
+      >
+        Check Out Our Social Media
+      </h1>
+    </footer>
+  );
 };
 
 export default Footer;
