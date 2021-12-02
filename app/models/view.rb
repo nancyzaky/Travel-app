b2b70eb60514953fd08b5529e021f447ca45a49b
Nancy Zaky <nancyzki@yahoo.com>
2021-12-02 14:11:52 -0800

    removed uniqueness from view model

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,5 +1,6 @@
 class View < ApplicationRecord
   validates :hotel_id, presence:true
-  validates :url, presence:true, uniqueness: true
+  validates :url, presence:true
+  # , uniqueness: true
   belongs_to :hotel
 end
