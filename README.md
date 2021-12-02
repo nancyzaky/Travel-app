0b549c2b48093939b68e2b185dc1e356087b76fe
Nancy Zaky <nancyzki@yahoo.com>
2021-12-02 14:44:56 -0800

    added description/ css

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-...
+TripIt is a booking web app I designed and built as a Flatiron school coding bootcamp project.
+Book hotel rooms, check near by activities and more.
+
+🛠 Installation & Set Up Install and use the correct version of Node using NVM
+
+Install gems by running -----> bundle install
+
+Install dependencies Make sure you cd into client folder
+
+npm install
+
+npm start
+
+🚀 Building and Running for Production Generate a full static production build
+
+npm run build Preview the site as it will appear once deployed
+
+npm run serve
+
+Features Im currently working on:
+
+-Design responsiveness and overall project styling..
+-Action mailer for confirming booking & activities reminder emails.
--- client/src/Prediction.js
+++ client/src/Prediction.js
@@ -13,24 +13,25 @@ const Prediction = ({ pred, index }) => {
         gridTemplateColumns: "repeat(3, 1fr)",
         gridTemplateRows: "1fr 1fr 1fr",
         justifyContent: "end",
-        height: "30vh",
+        height: "32vh",
         marginRight: "2rem",
+        paddingBottom: "2rem",
       }}
     >
       <ul style={{ display: "grid" }}>
-        <h4>Day {index + 1} </h4>
+        <h4 style={{ paddingBottom: "1rem" }}>Day {index + 1} </h4>
         <p>Humidity: {pred.humidity}</p>
         <p>Pressure: {pred.pressure}</p>
         <p>Wind Speed: {pred.wind_speed}</p>
       </ul>
-      <ul style={{ display: "grid" }}>
-        <h4>Temperature</h4>
+      <ul style={{ display: "grid", marginRight: "1rem" }}>
+        <h4 style={{ paddingBottom: "1rem" }}>Temperature</h4>
         <p>Average Temperature: {pred.temp.average} K</p>
         <p>Max Temperature {pred.temp.average_max}</p>
         <p>Min Temperature {pred.temp.average_min}</p>
       </ul>
       <ul style={{ display: "grid" }}>
-        <h4>Historical weather Record</h4>
+        <h4 style={{ paddingBottom: "1rem" }}>Historical weather Record</h4>
         <p>Max Temperature {pred.temp.record_max}</p>
         <p>Min Temperature {pred.temp.record_min}</p>
       </ul>
